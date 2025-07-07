39709a710815399e96e8d4d30a49f0212eb2f1b2
Bharani1504 <bharaniprasad286@gmail.com>
2025-07-08 03:57:03 +0530

    Until OurClient page is done

--- src/Pages/OurClientSection/OurClientSection.js
+++ src/Pages/OurClientSection/OurClientSection.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import './OurClientSection.css';
+
 import Eswari from './EaswariLogo.png';
 import VIT from './VITLogo.png';
 import VITAP from './VITAPLogo.png';
@@ -8,17 +10,21 @@ import KPR from './KPRLogo.png';
 const OurClientSection = () => {
   return (
     <div>
-        <div>OUR CLIENTS</div>
-        <div>We don't just build softwares, we build your business.</div>
-        <div>
-            <img src={Eswari} alt='Eswari Engineering College'/>
-            <img src={VIT} alt='Vellore Institute of Technology'/>
-            <img src={VITAP} alt='VIT-AP'/>
-            <img src={SRM} alt='SRM Institute of Science and Technology'/>
-            <img src={KPR} alt='KPR Institute of Engineering and Technology'/>
-        </div>
+      <div className="client-section">
+        <h2>OUR CLIENTS</h2>
+        <p className="subtitle">
+          We don't just build softwares, we build your business.
+        </p>
+      </div>
+      <div className="client-logos">
+        <img src={Eswari} alt='Eswari Engineering College'/>
+        <img src={VIT} alt='Vellore Institute of Technology'/>
+        <img src={VITAP} alt='VIT-AP'/>
+        <img src={SRM} alt='SRM Institute of Science and Technology'/>
+        <img src={KPR} alt='KPR Institute of Engineering and Technology'/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OurClientSection
+export default OurClientSection;
